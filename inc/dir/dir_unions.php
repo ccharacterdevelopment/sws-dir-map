0d13553e59b772f88fda57a2e45e73110f02dc60
Sharon Stromberg <sharon@ccharacter.com>
2021-02-03 17:22:48 -0500

    change func

--- inc/dir/dir_unions.php
+++ inc/dir/dir_unions.php
@@ -6,12 +6,10 @@ include "assets/functions_sws.php";
 
 if (isset($_GET['m'])) {$min=$_GET['m']; $_SESSION['sew']['min']=$_GET['m'];} else {$min='fam'; $_SESSION['sew']['min']="fam";}
 
-// echo sew_su_tags($min);
-
 $_SESSION['sew']['which']="fam";
 
+sws_style_links();
 
-include "assets/style_links.php";
 ?>
 <div style='width:100%'>
 <?php
