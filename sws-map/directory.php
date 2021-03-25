a9a9611a6506436ee89b45a4abd61c5dcdd0d702
Sharon Stromberg <sharon@ccharacter.com>
2021-03-25 15:06:32 -0400

    uncomment

--- sws-map/directory.php
+++ sws-map/directory.php
@@ -2,7 +2,7 @@
 //session_start();
 
 echo getcwd();
-require_once("../inc/dir/assets/Db.php");
+// require_once("../inc/dir/assets/Db.php");
 //require_once plugin_dir_path(__FILE__).'inc/dir/assets/Db.php';
 //require_once("../../functions/Db.php");
 //require_once("../../functions/functions_sew.php");
